155b45746eabcaf3bd7ea3da546d590d780d1a11
pBouillon <pro.pierre.bouillon@openmailbox.org>
2018-01-07 12:33:59 +0100

    implemented commands

--- cli.py
+++ cli.py
@@ -30,14 +30,26 @@ from db_handler import DB_Handler
 import getpass
 from getpass import getpass
 
-CMD = 'pytodo> '
+
+CMD_INPUT     = 'pytodo> '
+CMD_PREFIX    = '/'
+
+CMD_ADD    = '/a'
+CMD_DONE   = '/d'
+CMD_HELP   = '/h'
+CMD_LIST   = '/l'
+CMD_QUIT   = '/q'
+CMD_REMOVE = '/r'
+
 COMMANDS = [
-    '/h', # help
-    '/a', # add
-    '/d', # done
-    '/r', # remove
-    '/q'  # quit
+    CMD_ADD ,
+    CMD_DONE,
+    CMD_HELP,
+    CMD_LIST,
+    CMD_QUIT,
+    CMD_REMOVE
 ]
+
 
 class Cli:
     """
@@ -51,71 +63,113 @@ class Cli:
     def __poll(self):
         """
         """
-        print('--- SUCCESSFULLY CONNECTED ---')
+        print('\n--- SUCCESSFULLY CONNECTED ---\n')
         while True:
-            inp = input(CMD)
-            if inp.startswith('/'):
-                cmd = inp[:2]
+            inp = input(CMD_INPUT)
+            if inp.startswith(CMD_PREFIX):
+                cmd = inp.split()[0]
                 if cmd in COMMANDS:
-                    if cmd == '/a':
+                    if cmd == CMD_LIST :
+                        self.__task_list()
+                    elif cmd == CMD_ADD:
                         self.__task_add(inp)
-                    elif cmd == '/d':
+                    elif cmd == CMD_DONE:
                         self.__task_done(inp)
-                    elif cmd == '/r':
+                    elif cmd == CMD_REMOVE:
                         self.__task_remove(inp)
-                    elif cmd == '/h':
+                    elif cmd == CMD_HELP :
                         self.__print_help()
                     else:
                         self.__quit_app()
                 else:
-                    print('Unhandled command type /h for help')
+                    print('Unhandled command type ' + CMD_HELP +' for help')
             else:
-                print('Commands must start with \'/\'')
-        self.__quit_app()
+                print('Commands must start with \'' + CMD_PREFIX + '\'')
 
     def __print_help(self):
         """
         """
         helper = 'Available commands:\n'
-        helper+= '\t/a <task_name> --- add a new task\n'
-        helper+= '\t/d <task_name> --- pass the task to \'done\'\n'
-        helper+= '\t/r <task_name> --- delete task\n'
-        helper+= '\t/h             --- displays help\n'
-        helper+= '\t/q             --- quit\n'
+        helper+= '\t'+CMD_ADD   +' [desc] .. add a new task\n'
+        helper+= '\t'+CMD_DONE  +' id ...... pass the task to \'done\'\n'
+        helper+= '\t'+CMD_LIST  +' ......... list all tasks\n'
+        helper+= '\t'+CMD_REMOVE+' id ...... remove the task\n'
+        helper+= '\t'+CMD_HELP  +' ......... displays help\n'
+        helper+= '\t'+CMD_QUIT  +' ......... quit\n'
         print(helper)
 
 
     def __task_add(self, cmd):
         """
         """
-        pass
+        description = ''
+
+        if len(cmd) > 3 :
+            description = cmd[3:]
+        else:
+            print('Please specify a description: ')
+            description = input('> ')
+
+        self.__db.task_register(description)
+        print('Task added.\n')
 
     def __task_done(self, cmd):
         """
         """
-        pass
+        if len(cmd.split()) < 2:
+            print('Missing parameter, see ' + CMD_HELP + '.\n')
+        else:
+            task_id = cmd.split()[1]
+            self.__db.task_done(task_id)
+            print('Task status changed.\n')
+
+    def __task_list(self):
+        """
+        """
+        tasks = self.__db.task_list()
+        if len(tasks) == 0 :
+            print('No task planified yet, add one with ' + CMD_ADD)
+        else:
+            print('Registered task(s):')
+            for task in tasks:
+                task_sum = '\t'
+                if task[2] == 0:
+                    task_sum += '[ ] '
+                else:
+                    task_sum += '[x] '
+                task_sum += '(' + str(task[0]) + ') '
+                task_sum += task[1]
+                print (task_sum)
+            print('')
 
     def __task_remove(self, cmd):
         """
         """
-        pass
+        if len(cmd.split()) < 2:
+            print('Missing parameter, see ' + CMD_HELP + '.\n')
+        else:
+            task_id = cmd.split()[1]
+            self.__db.task_delete(task_id)
+            print('Task removed.\name')
 
     def __quit_app(self):
         """
         """
         self.__db.disconnect()
+        print('--- SUCCESSFULLY DISCONNECTED ---')
+        exit()
 
     def __start(self):
         """
         """
-        if not self.__db.check_user(): # user doest not exists
+        if not self.__db.user_exists(): # user doest not exists
             msg = 'It seems that no users are registered \n'
-            msg+= 'Do you want to sign in ? (y/n)'
+            msg+= 'Do you want to sign in ?'
             print(msg)
 
             inp = -1
             while inp not in ['y', 'n']:
-                inp = input(CMD)
+                inp = input('(y/n) > ')
 
             if inp == 'y':
                 print('\nPlease enter your informations: ')
@@ -129,7 +183,7 @@ class Cli:
                     verif = getpass('Repeat password> ')
 
                     if psswd != verif:
-                        print('Passwords don\'t match')
+                        print('\nPasswords don\'t match')
 
                 self.__db.user_register(name, psswd)
 
@@ -143,5 +197,7 @@ class Cli:
                 name  = input('User name: ')
                 psswd = getpass('Password: ')
                 connected = self.__db.connect(name, psswd)
+                if not connected:
+                    print('\nLogin failed.')
 
         self.__poll() # start the app
